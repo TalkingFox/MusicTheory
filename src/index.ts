345ee90382f617ea4781b6255b0d7315cf152287
Mike Wilkinson <mwilkinson@imo-online.com>
2022-10-06 21:19:20 -0500

    Import tone. Button is visible that produces a sound when clicked

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("TypeScript Practice");
+import * as Tone from 'tone'
+
+var button = document.createElement('button')
+button.textContent = "Make Sound"
+button.addEventListener('click', () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease('C4', '8n');
+});
+
+document.body.appendChild(button);
+

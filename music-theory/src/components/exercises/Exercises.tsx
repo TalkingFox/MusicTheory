66fa42521086db0195f2e4e1bd4213dac918fe24
Mike Wilkinson <mwilkinson@imo-online.com>
2025-03-09 21:28:58 -0500

    Implement "show notes" for exercise

--- music-theory/src/components/exercises/Exercises.tsx
+++ music-theory/src/components/exercises/Exercises.tsx
@@ -1,8 +1,9 @@
-import { act, useState } from "react";
+import { act, useEffect, useRef, useState } from "react";
 import { SoundService } from "../../services/sound-service/SoundService";
 import exerciseData from './Exercises.json';
 import './Exercises.css';
 import { SoundServiceNote } from "../../services/sound-service/SoundServiceNote";
+import { Factory } from "vexflow";
 
 export interface ExercisesProps {
     soundService: SoundService
@@ -11,15 +12,26 @@ export interface ExercisesProps {
 const Exercises = ({ soundService }: ExercisesProps) => {
     const [exerciseIndex, setExerciseIndex] = useState(0);
     const [measureIndex, setMeasureIndex] = useState(0);
+    const [areNotesVisible, setAreNotesVisible] = useState(false);
 
     const activeExercise = exerciseData[exerciseIndex];
+    const factoryRef = useRef<Factory>(null);
+
 
     const loadPreviousExercise = () => {
+        setAreNotesVisible(false);
+        if (factoryRef.current) {
+            factoryRef.current.getContext().clear();
+        }
         setExerciseIndex(exerciseIndex - 1);
         setMeasureIndex(0);
     };
 
     const loadNextExercise = () => {
+        setAreNotesVisible(false);
+        if (factoryRef.current) {
+            factoryRef.current.getContext().clear();
+        }
         setExerciseIndex(exerciseIndex + 1);
         setMeasureIndex(0);
     };
@@ -33,6 +45,74 @@ const Exercises = ({ soundService }: ExercisesProps) => {
     };
 
     const showNotes = () => {
+        setAreNotesVisible(true);
+        const height = 300;
+        const width = window.innerWidth * 0.75;
+
+        if (!factoryRef.current) {
+            factoryRef.current = new Factory({
+                renderer: {
+                    elementId: 'notes-container',
+                    height: height,
+                    width: width
+                }
+            });
+        }
+
+        const factory = factoryRef.current;
+
+        const score = factory.EasyScore();
+        score.set({ time: activeExercise.timeSignature });
+
+        const measureWidth = width / activeExercise.measures.length;
+        activeExercise.measures.forEach((measure, index) => {
+            const system = factory.System({
+                x: measureWidth * index,
+                y: 0,
+                width: measureWidth,
+                spaceBetweenStaves: 10
+            });
+
+            const syntaxNotes = measure.map((note) => {
+                const parts = note.split('-');
+                let noteKind = '/q';
+                switch (parts[1]) {
+                    case '1n':
+                        noteKind = '/w';
+                        break;
+                    case '2n':
+                        noteKind = '/h';
+                        break;
+                    case '4n':
+                        noteKind = '/q';
+                        break;
+                    case '8n':
+                        noteKind = '/8';
+                        break;
+                }
+                return `${parts[0]}${noteKind}`;
+            }).join(',');
+
+            const staff = system.addStave({
+                voices: [
+                    score.voice(score.notes(
+                        syntaxNotes, { clef: 'treble' }
+                    ))
+                ]
+            });
+
+            if (index == 0) {
+                staff.addClass('treble')
+                    .addTimeSignature(activeExercise.timeSignature)
+                    .addKeySignature(activeExercise.signature);
+
+                system.addConnector('brace');
+                system.addConnector('singleRight');
+                system.addConnector('singleLeft');
+            }
+        });
+
+        factory.draw();
 
     };
 
@@ -52,6 +132,10 @@ const Exercises = ({ soundService }: ExercisesProps) => {
         soundService.PlayNotes(melodyNotes);
     };
 
+    const notesClasses = ['notes-container'];
+    if (!areNotesVisible) {
+        notesClasses.push('notes-shaded');
+    }
 
     return <>
         <div className="exercise-description-container">
@@ -59,7 +143,7 @@ const Exercises = ({ soundService }: ExercisesProps) => {
             <span>Exercise {exerciseIndex + 1}</span>
             <button onClick={loadNextExercise} disabled={exerciseIndex == exerciseData.length - 1}>&gt;</button>
         </div>
-        <div className="notes-container notes-shaded"></div>
+        <div id='notes-container' className={notesClasses.join(' ')}></div>
         <div className="exercise-measure-container">
             <button onClick={loadPreviousMeasure} disabled={measureIndex == 0}>&lt;</button>
             <button onClick={playMeasure}>Play Measure {measureIndex + 1}</button>
